44720aa14e615ef4ce820feb7467ab654581d676
fadeddman <tough.moon2126@fadedpotato.com>
2026-08-23 17:08:13 -0700

    solved day 3 part 1

---
+++ 2015/day_3/day_3.cpp
@@ -0,0 +1,89 @@
+// Advent of Code 2015 - Day 3
+#include "aoc.hpp"
+
+#include <iostream>
+#include <map>
+#include <tuple>
+
+class Point{
+public:
+    Point(int x, int y) : x(x), y(y) {}
+
+    bool operator<(const Point& other) const {
+        return std::tie(x, y) < std::tie(other.x, other.y);
+    } 
+
+    void goLeft()
+    {
+        x = x - 1;
+    }
+
+    void goRight()
+    {
+        x = x + 1;
+    }
+
+    void goUp()
+    {
+        y = y + 1;
+    }
+
+    void goDown()
+    {
+        y = y - 1;
+    }
+
+private:
+    int x;
+    int y;
+};
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input file>\n";
+        return 1;
+    }
+
+    const auto lines = aoc::read_lines(argv[1]);
+
+    // Part 1
+    {
+        Point santa(0,0);
+        std::map<Point, int> delivered;
+        delivered[santa] = 1;
+        int total_visited = 1;
+        for (unsigned long i = 0; i < lines.size(); i++)
+        {
+            for (char c : lines[i])
+            {
+                switch (c){
+                    case '>':
+                        santa.goLeft();
+                        break;
+                    case '<':
+                        santa.goRight();
+                        break;
+                    case '^':
+                        santa.goUp();
+                        break;
+                    case 'v':
+                        santa.goDown();
+                        break;
+                    default:
+                        break;
+                }
+                if (!delivered.contains(santa))
+                {
+                    delivered[santa] = 1;
+                    total_visited++;
+                }
+            }
+        }
+        std::cout << "Part 1: " << total_visited << '\n';
+    }
+
+    // Part 2
+    {
+        std::cout << "Part 2: " << 0 << '\n';
+    }
+}
